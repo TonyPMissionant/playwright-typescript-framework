6fd20c3b3ff8e8396a9d17b56ab9cbf915b883ae
Tony <missionant@hotmail.co.uk>
2026-08-03 18:12:11 +0100

    Remove unused page references from checkout pages

--- pages/CheckoutOverviewPage.ts
+++ pages/CheckoutOverviewPage.ts
@@ -1,17 +1,14 @@
 import { Page, Locator } from '@playwright/test';
 
 export class CheckoutOverviewPage {
-    readonly page: Page;
     readonly pageHeading: Locator;
     readonly backpackName: Locator;
     readonly backpackPrice: Locator;
     readonly finishButton: Locator;
 
     constructor(page: Page) {
-        this.page = page;
-
         this.pageHeading = page.locator('[data-test="title"]');
-        this.backpackName = page.getByText('Sauce Labs Backpack',{ exact: true });
+        this.backpackName = page.getByText('Sauce Labs Backpack', { exact: true });
         this.backpackPrice = page.locator('[data-test="inventory-item-price"]');
         this.finishButton = page.locator('[data-test="finish"]');
     }
