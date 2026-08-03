59250fac06372c3c4e6939b73c1cba2825b14e16
Tony <missionant@hotmail.co.uk>
2026-08-03 15:54:32 +0100

    Removed unused page reference from login page

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -2,23 +2,21 @@ import { Locator, Page } from '@playwright/test';
 
 export class LoginPage {
 
-    private readonly page: Page;
-
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
     readonly errorMessage: Locator;
 
-constructor(page: Page) {
-    this.page = page;
-    this.usernameInput = page.getByPlaceholder('Username');
-    this.passwordInput = page.getByPlaceholder('Password');
-    this.loginButton = page.getByRole('button', { name: 'Login' });
-    this.errorMessage = page.locator('[data-test="error"]');  
-}
-async login(user: string, passs: string) {
-    await this.usernameInput.fill(user);
-    await this.passwordInput.fill(passs);
-    await this.loginButton.click();
-}
+    constructor(page: Page) {
+        this.usernameInput = page.getByPlaceholder('Username');
+        this.passwordInput = page.getByPlaceholder('Password');
+        this.loginButton = page.getByRole('button', { name: 'Login' });
+        this.errorMessage = page.locator('[data-test="error"]');
+    }
+
+    async login(user: string, pass: string) {
+        await this.usernameInput.fill(user);
+        await this.passwordInput.fill(pass);
+        await this.loginButton.click();
+    }
 }
